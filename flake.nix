bb39366e0880de17bbc14d6b1abc6d0448a311c3
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-10-26 17:54:52 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
       in rec {
         packages = {
           webserver = pkgs.writeScriptBin "server" ''
+            touch index.htm
             while true; do { \
               echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.htm)\r\n\r\n"; \
               cat index.htm; } | nc -l -p 8080 ; \
