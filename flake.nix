c15e558fc63ba373092bffbc433bd75cdbc4d293
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-10-26 17:59:30 +0200

    node

--- flake.nix
+++ flake.nix
@@ -21,11 +21,7 @@
       in rec {
         packages = {
           webserver = pkgs.writeScriptBin "server" ''
-            touch index.htm
-            while true; do { \
-              echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.htm)\r\n\r\n"; \
-              cat index.htm; } | nc -l -p 8080 ; \
-            done
+            ${pkgs.nodejs_22}/bin/node ${./src/index.js}
           '';
           default = packages.webserver;
         };
